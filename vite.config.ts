916331b735ec90b6d11f8f94315175c841cad506
DANIEL ENOC AMAYA AMAYA <130795116+danielamaya1908@users.noreply.github.com>
2025-07-20 12:25:07 -0500

    t1

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
+  base: "/",
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  preview: {
+    port: 4173,
+    host: "0.0.0.0",
+  },
+  build: {
+    outDir: "dist",
+  },
+});
